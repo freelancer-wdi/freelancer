53286cebe0a51a0a3b40916f699feb1310cdcb00
Salman778 <alk-333@hotmail.com>
2019-09-21 19:45:09 +0300

    let path after sing in to index of projects

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -20,6 +20,12 @@ class Customers::SessionsController < Devise::SessionsController
 
   # protected
 
+  # The path used after sign in.
+  def after_sign_in_path_for(resource)
+    super(resource)
+    '/projects'
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
