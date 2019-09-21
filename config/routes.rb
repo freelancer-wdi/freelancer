885f6e4bedd455234ade6c9edd5fce595ce810bf
Salman778 <alk-333@hotmail.com>
2019-09-21 18:21:47 +0300

    overridden routes for generated controller of custmers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  devise_for :customers, controllers: { registrations: 'customers/registrations',
+                                     sessions: 'customers/sessions', unlocks: 'customers/unlocks',
+                                     passwords: 'customers/passwords', confirmations: 'customers/confirmations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
