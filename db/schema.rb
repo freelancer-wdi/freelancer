e16ef37070fc03141037a36b6b62f746bf6348ac
Salman778 <alk-333@hotmail.com>
2019-09-21 18:45:19 +0300

    migrate Projects into database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_21_145807) do
+ActiveRecord::Schema.define(version: 2019_09_21_153358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 2019_09_21_145807) do
     t.index ["phone"], name: "index_customers_on_phone", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
     t.index ["username"], name: "index_customers_on_username", unique: true
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "status"
+    t.text "description"
+    t.text "requirements"
+    t.decimal "budget"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_projects_on_name", unique: true
   end
 
 end
