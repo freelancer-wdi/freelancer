5dae41c9bd403664d409621c648dc748d99352e8
Salman778 <alk-333@hotmail.com>
2019-09-21 19:52:32 +0300

    let the root of url path to index of projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "projects#index"
   resources :projects
   devise_for :customers, controllers: { registrations: 'customers/registrations',
                                      sessions: 'customers/sessions', unlocks: 'customers/unlocks',
