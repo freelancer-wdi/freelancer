064fbdff934f98ae39d6f20949099013200b05d1
Salman778 <alk-333@hotmail.com>
2019-09-23 11:42:28 +0300

    migrate programmer into database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_21_154858) do
+ActiveRecord::Schema.define(version: 2019_09_23_083437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,24 @@ ActiveRecord::Schema.define(version: 2019_09_21_154858) do
     t.index ["phone"], name: "index_customers_on_phone", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
     t.index ["username"], name: "index_customers_on_username", unique: true
+  end
+
+  create_table "programmers", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "city", default: "", null: false
+    t.string "phone", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_programmers_on_email", unique: true
+    t.index ["phone"], name: "index_programmers_on_phone", unique: true
+    t.index ["reset_password_token"], name: "index_programmers_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_programmers_on_username", unique: true
   end
 
   create_table "projects", force: :cascade do |t|
