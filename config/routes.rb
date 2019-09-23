05571ea04578fa75e6a6139daae7e3a2ffa3e734
Salman778 <alk-333@hotmail.com>
2019-09-23 11:35:56 +0300

    genrate devise model Programmer by run this command #rails generate devise Programmer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :programmers
   root "projects#index"
   resources :projects
   devise_for :customers, controllers: { registrations: 'customers/registrations',
