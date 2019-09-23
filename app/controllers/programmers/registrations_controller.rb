ecf9917b3bd801c11a293d374651351915e4f0b1
Salman778 <alk-333@hotmail.com>
2019-09-23 14:28:22 +0300

    path used after sign in or update is to portfolio show

--- app/controllers/programmers/registrations_controller.rb
+++ app/controllers/programmers/registrations_controller.rb
@@ -51,9 +51,16 @@ class Programmers::RegistrationsController < Devise::RegistrationsController
   end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    super(resource)
+    redirect_to portfolio_path(current_programmer.id)
+  end
+
+  # The path used after update.
+  def after_update_path_for(resource)
+    super(resource)
+    redirect_to portfolio_path(current_programmer.id)
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
