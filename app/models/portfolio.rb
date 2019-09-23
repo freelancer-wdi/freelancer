17befdaa8f22f46896d5ae1d94d423aeba9990f2
Salman778 <alk-333@hotmail.com>
2019-09-23 12:58:38 +0300

    let programmer uniqe so programmer can only have one portfolio add let programmer presence

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,6 @@
 class Portfolio < ApplicationRecord
     belongs_to :programmer
+    validates_uniqueness_of :programmer_id, :message => "programmer can only        
+                                     have one Portfolio"
+    validates :programmer_id, presence: true
 end
