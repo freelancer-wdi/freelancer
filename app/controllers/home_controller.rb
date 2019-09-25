c109eea1e23f6bee8fc55d4f447abf774181f1f2
Salman778 <alk-333@hotmail.com>
2019-09-25 05:07:30 +0300

    let the root of programmer is offers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
     def index
         if programmer_signed_in?
-            redirect_to portfolios_path
+            redirect_to offers_path
         elsif customer_signed_in?
             redirect_to projects_path
         end
