b49133ce42aac2b2a0a7692443ab761907d4119c
Salman778 <alk-333@hotmail.com>
2019-09-25 22:47:50 +0300

    let is_accepted false by default

--- db/migrate/20190925194337_add_is_accepted_to_offer.rb
+++ db/migrate/20190925194337_add_is_accepted_to_offer.rb
@@ -1,5 +1,5 @@
 class AddIsAcceptedToOffer < ActiveRecord::Migration[5.2]
   def change
-    add_column :offers, :is_accepted, :boolean
+    add_column :offers, :is_accepted, :boolean, default: false
   end
 end
