bf05474357d7f2deb08e6876170cdd23d3648220
Salman778 <alk-333@hotmail.com>
2019-09-25 22:48:42 +0300

    migrate file that add is_accepted coulmn to offers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_225812) do
+ActiveRecord::Schema.define(version: 2019_09_25_194337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_09_24_225812) do
     t.datetime "updated_at", null: false
     t.bigint "programmer_id"
     t.bigint "project_id"
+    t.boolean "is_accepted", default: false
     t.index ["programmer_id"], name: "index_offers_on_programmer_id"
     t.index ["project_id"], name: "index_offers_on_project_id"
   end
