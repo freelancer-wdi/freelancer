b76d45af5782b22abd9df7ee2e6764678d1d2d46
Salman778 <alk-333@hotmail.com>
2019-09-29 23:43:21 +0300

    add path for update status of project to let customr update his project

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,25 @@
 Rails.application.routes.draw do
+
+  root "home#index"
+
+
   get '/offers/new/:id', to: 'offers#new', as: 'new_offer'
-  get 'projects/:id/:offer_id/accepted', to: 'projects#offer_accepted', as:'offer_accepted'
   resources :offers, except: :new
+
+
+  get 'projects/:id/:offer_id/accepted', to: 'projects#offer_accepted', as:'offer_accepted'
+  get 'projects/:id/status', to: 'projects#update_status', as:'status_update'
+  resources :projects
+
+
   resources :portfolios, only: [:index, :edit, :update]
+
+
   devise_for :programmers, controllers: { registrations: 'programmers/registrations',
                                           sessions: 'programmers/sessions', unlocks: 'programmers/unlocks',
                                           passwords: 'programmers/passwords', confirmations: 'programmers/confirmations'}
-  root "home#index"
-  resources :projects
+
+
   devise_for :customers, controllers: { registrations: 'customers/registrations',
                                      sessions: 'customers/sessions', unlocks: 'customers/unlocks',
                                      passwords: 'customers/passwords', confirmations: 'customers/confirmations'}
