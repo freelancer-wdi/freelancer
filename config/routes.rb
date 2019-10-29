64d6f52765dcc89fb70d8726de66c97f44a2e1f8
Salman778 <alk-333@hotmail.com>
2019-10-29 10:00:56 +0300

    genrate Modle SoftwareEnginerrings by devise and the add extra columns

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :software_engineerings
   root "home#index"
 
 
---
+++ test/models/software_engineering_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SoftwareEngineeringTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
